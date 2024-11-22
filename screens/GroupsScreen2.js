524b0b07f4d6cf59cf84e40d8a38c6b87e51489e
Du4rteRodrigues <duarte.m.rodrigues@sapo.pt>
2024-11-22 22:42:39 +0000

    group screen changes

---
+++ screens/GroupsScreen2.js
@@ -0,0 +1,69 @@
+// screens/GroupsScreen.js
+import React from "react";
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from "react-native";
+import Button from "../components/Button";
+import Icon from "react-native-vector-icons/Ionicons";
+import { commonStyles, screenHeight, screenWidth } from "../styles/commonStyles";
+import GroupsListComponent from "../components/GroupListComponent";
+
+const GroupsScreen  = ({ navigation, isHiddenVisible, toggleHiddenElement }) => {
+
+  const handleGroupClick = (group) => {
+    navigation.navigate("GroupDetail", { group });
+  };
+
+  return (
+    <View style={commonStyles.container}>
+      <TouchableOpacity
+        style={styles.backButton} 
+        onPress={() =>
+          navigation.goBack()
+        }
+      >
+        <Icon name="arrow-back" size={30} color="white" />
+      </TouchableOpacity>
+      <Text style={styles.titleText}>Groups</Text>
+      <TextInput style={styles.input} placeholder="Search Groups" />
+      <View style={styles.content_box}>
+        <GroupsListComponent />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  content_box: {
+    width: screenWidth,
+    height: screenHeight * 0.70,
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "rgba(255, 255, 255, 0.40)",
+    borderRadius: 67,
+  },
+  titleText: {
+    marginTop: "10%",
+    fontSize: 35,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 10,
+  },
+  input: {
+    fontSize: 15,
+    padding: 15,
+    width: "95%",
+    borderRadius: 12,
+    marginBottom: "5%",
+    backgroundColor: "#fff",
+  },
+  backButton: {
+    position: "absolute",
+    top: "8%",
+    left: "10%",
+  },
+});
+
+export default GroupsScreen;
---
+++ utils/groupPics.js
@@ -0,0 +1,7 @@
+  export default {
+  FOOTBALL: require("../assets/group_pics/football.webp"),
+  TENNIS: require("../assets/group_pics/tennis.webp"),
+  LITERATURE: require("../assets/group_pics/literature.webp"),
+  BASKETBALL: require("../assets/group_pics/basketball.webp"),
+  MUSIC: require("../assets/group_pics/music.webp"),
+};
